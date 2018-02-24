f6f9174cca2b6fc9830372d3c5a31bced625c5a5
Justin B <justinmbyers@gmail.com>
2018-02-24 12:09:15 -0600

    add Connection Manager

--- Tower_Defense_ComCap/Assets/_SCRIPTS/Manager_Connections.cs
+++ Tower_Defense_ComCap/Assets/_SCRIPTS/Manager_Connections.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Manager_Connections : MonoBehaviour {
-
+    public string gameVersion;
 	// Use this for initialization
 	void Start () {
-		
+        PhotonNetwork.ConnectUsingSettings(gameVersion);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void JoinGame()
+    {
+        RoomOptions ro = new RoomOptions();
+        ro.MaxPlayers = 8;
+        PhotonNetwork.JoinOrCreateRoom("Default Room", ro, null);
+    }
+    public void OnJoinedRoom()
+    {
+        Debug.Log("Joined Room");
+    }
 }
